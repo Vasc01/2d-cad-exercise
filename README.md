29276ee57ff1214622a7f0abf5c7f0866d15bbb5
Vasc01 <vasil.andreev.workspace@gmail.com>
2023-11-09 14:07:19 +0200

    (#9) - Readme update

--- README.md
+++ README.md
@@ -2,3 +2,43 @@
 The goal of this project is to exercise and demonstrate the good practices in software development.
 
 For more information on the project and contribution guidelines see the project Wiki.
+
+# How to Run
+
+- Technical requirements:
+  - python 3
+  - 'windows-curses' if you run the program in Windows. _curses_ is the text based user interface module that should be 
+  already available on macOS and Linux
+- The textbased user interface relies on the functionalities of the terminal. The program is started from the
+terminal using the syntax `C:\path\to\project\folder> python main.py`.
+    - Note: Some functionalities of the program may not be supported in all terminals. Appearance may differ as well.
+    - Note: For the start of the program extend the terminal window size to relatively big portion of the screen.
+  If the terminal is too small it will not offer enough characters for all the interface features. This will result in
+  an error on start.
+
+# How to Use
+
+This program is at its current state entirely keyboard operated. 
+
+The screen is split in multiple windows in the common arrangement for CAD software. The command input area 
+is the lowest window labeled _Command_. Above it is the _Prompt_ window which will guide the user
+what kind of input is expected. Most of the commands require multiple steps to complete. At the end of
+the command execution a sound is audible.
+
+Use the following shortcuts to initiate commands and then follow the instruction in the _Prompt_ window.
+
+
+| Command      | Shortcut |
+|--------------|----------|
+| Add Element  | `a`      |
+| Del Element  | `d`      |
+| Insert Shape | `i`      |
+| Clear All    | `c`      |
+| Move         | `m`      |
+| Rotate       | `r`      |
+| Mirror       | `mi`     |
+| Scale        | `s`      |
+
+Many of the commands require navigation with the cursor and picking or placing objects in the _Canvas_ or _Palette_
+window. Navigation is done with the numlock arrows. Selection is executed with the number _5_ button in the center,
+end of navigation/selection is achieved with the _home/7_ button.
--- frontend/initial_data.py
+++ frontend/initial_data.py
@@ -3,6 +3,41 @@ from backend.transformer import CartesianTransformer
 
 transformer = CartesianTransformer()
 
+# stores elements for commands
+temporary_group = Group()
+temporary_group.set_transformer(transformer)
+
+# elements for palette
+palette_elements = [
+    Element(2, 0).set_symbol("&"),
+    Element(4, 0).set_symbol("#"),
+    Element(6, 0).set_symbol("@"),
+    Element(2, 1).set_symbol("%"),
+    Element(4, 1).set_symbol("X"),
+    Element(6, 1).set_symbol("0")
+]
+
+palette = Group()
+palette.set_transformer(transformer)
+for el in palette_elements:
+    palette.add(el)
+
+# predefined figures
+z_list = [(30, 10), (31, 10), (32, 10), (33, 10), (34, 10),
+          (34, 11),
+          (34, 12), (35, 12), (36, 12), (37, 12), (38, 12)]
+
+square_list = [(50, 10), (51, 10), (52, 10), (53, 10), (54, 10),
+               (50, 11), (54, 11),
+               (50, 12), (54, 12),
+               (50, 13), (54, 13),
+               (50, 14), (51, 14), (52, 14), (53, 14), (54, 14)]
+
+predefined_square = Group(transformer=transformer)
+for xy in square_list:
+    element = Element(xy[0], xy[1]).set_transformer(transformer).set_symbol("X")
+    predefined_square.add(element)
+
 # Elements preexistent on canvas
 element_1 = Element(20, 10).set_symbol("@")
 element_2 = Element(22, 10).set_symbol("%")
@@ -12,62 +47,3 @@ canvas.set_transformer(transformer)
 canvas.add(element_1)
 canvas.add(element_2)
 canvas.add(element_3)
-
-# stores elements for commands
-temporary_group = Group()
-temporary_group.set_transformer(transformer)
-
-# elements for palette
-element_A = Element(0, 0).set_symbol("&")
-element_B = Element(0, 1).set_symbol("#")
-palette = Group()
-palette.set_transformer(transformer)
-palette.add(element_A)
-palette.add(element_B)
-
-# place cursor to center of window
-# tetris
-z_list = [(30, 10), (31, 10), (32, 10), (33, 10), (34, 10),
-          (34, 11),
-          (34, 12), (35, 12), (36, 12), (37, 12), (38, 12)]
-square_list = [(50, 10), (51, 10), (52, 10), (53, 10), (54, 10),
-               (50, 11), (54, 11),
-               (50, 12), (54, 12),
-               (50, 13), (54, 13),
-               (50, 14), (51, 14), (52, 14), (53, 14), (54, 14)]
-
-L_list = [(80, 10),]
-
-# circle
-circle_list = []
-
-# smiley
-smiley_list = []
-
-
-# place cursor to center of window
-# tetris
-z_list = [(30, 10), (31, 10), (32, 10), (33, 10), (34, 10),
-          (34, 11),
-          (34, 12), (35, 12), (36, 12), (37, 12), (38, 12)]
-
-square_list = [(50, 10), (51, 10), (52, 10), (53, 10), (54, 10),
-               (50, 11), (54, 11),
-               (50, 12), (54, 12),
-               (50, 13), (54, 13),
-               (50, 14), (51, 14), (52, 14), (53, 14), (54, 14)]
-
-L_list = [(80, 10)]
-
-# circle
-circle_list = []
-
-# smiley
-smiley_list = []
-
-
-predefined_square = Group(transformer=transformer)
-for xy in square_list:
-    element = Element(xy[0], xy[1]).set_transformer(transformer).set_symbol("X")
-    predefined_square.add(element)
-# predefined_square.add(Element(52, 12).set_transformer(transformer).set_symbol("+"))
